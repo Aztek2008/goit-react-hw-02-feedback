9a1101f6240f4e04064b21268949198e68eb7673
Oleksii Skopiuk <aztek2008@gmail.com>
2020-08-02 16:40:37 +0300

    Prework commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Components } from "react";
+import "./App.css";
+
+class App extends Components() {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return {
+      App,
+    };
+  }
+}
+
+export default App;
