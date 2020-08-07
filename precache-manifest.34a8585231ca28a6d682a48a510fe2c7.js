7a71ece7f192cac1e2611f584e48b755768c1ace
Aztek2008 <Aztek2008@gmail.com>
2020-08-07 22:28:38 +0300

    Updates

---
+++ precache-manifest.34a8585231ca28a6d682a48a510fe2c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa3e8e007c13f1a3087d6964575b7eb",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "47482d067835af215e6f",
+    "url": "/goit-react-hw-02-feedback/static/css/main.0441c589.chunk.css"
+  },
+  {
+    "revision": "870c6dddbd7d853bcb07",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0af00a61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.0af00a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47482d067835af215e6f",
+    "url": "/goit-react-hw-02-feedback/static/js/main.6f3ad946.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-feedback/precache-manifest.52c3fddf3fe44aa35a0b37a42cfa0cea.js"
+  "/goit-react-hw-02-feedback/precache-manifest.34a8585231ca28a6d682a48a510fe2c7.js"
 );
 
 self.addEventListener('message', (event) => {
